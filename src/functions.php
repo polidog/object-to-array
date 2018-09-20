2af87cc7b27346b521cd344c4e7d64313ddfe97c
polidog <polidogs@gmail.com>
2018-09-21 02:58:30 +0900

    fix strict bug

--- src/functions.php
+++ src/functions.php
@@ -6,14 +6,15 @@ namespace Polidog\ObjectToArray;
 
 /**
  * @param mixed $data
+ * @param bool $strict
  *
  * @return mixed
  */
-function object_to_array($data)
+function object_to_array($data, bool $strict = true)
 {
     if (true === \is_object($data)) {
         if (method_exists($data, '__toArray')) {
-            return $data->__toArray();
+            return $data->__toArray($strict);
         }
 
         if (method_exists($data, '__toString')) {
@@ -23,7 +24,7 @@ function object_to_array($data)
         $array = [];
         foreach ((array) $data as $key => $value) {
             $key = preg_replace('/\000(.*)\000/', '', $key);
-            $array[underscore($key)] = object_to_array($value);
+            $array[underscore($key)] = object_to_array($value, $strict);
         }
 
         return $array;
@@ -32,7 +33,7 @@ function object_to_array($data)
     if (true === \is_array($data)) {
         $stack = [];
         foreach ($data as $key => $value) {
-            $stack[underscore($key)] = object_to_array($value);
+            $stack[underscore($key)] = object_to_array($value, $strict);
         }
 
         return $stack;
